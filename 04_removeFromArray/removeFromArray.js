93010cb2e803aa2c97f385f73c955b5c1c92db28
Wetbaguette <yesseniapan@hotmail.com>
2022-10-08 00:06:52 -0500

    pass removeFromArray test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    let newArr = arr;
+      for (let i = 0; i < remove.length; i++) {
+          if (arr.includes(remove[i])) {
+              if (arr.indexOf(remove[i]) < 0) {
+                  return;
+              } else {
+                  arr.splice(arr.indexOf(remove[i]), 1);
+              }
+          };
+        }    
+    return newArr;
 };
 
 // Do not edit below this line
