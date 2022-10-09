1f20296dc4137facb84198c7a5d9845b32bc83ed
Wetbaguette <yesseniapan@hotmail.com>
2022-10-09 14:46:49 -0500

    pass sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let totalSum = 0
+      if (typeof firstNum === "number" && typeof secondNum === "number") {
+          if (firstNum < 0 || secondNum < 0) {
+              totalSum = "ERROR"
+          } else if (firstNum >= secondNum) {
+              for (let i = secondNum; i <= firstNum; i++) {
+                  totalSum += i;
+              }
+          } else if (firstNum <= secondNum) {
+              for (let i = firstNum; i <= secondNum; i++) {
+                  totalSum += i;
+              }
+          }
+      } else {
+        totalSum = "ERROR"
+      }    
+    return totalSum;
 };
 
 // Do not edit below this line
