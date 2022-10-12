f52b439c6f317dd910676b84cf9780708b85d32e
Wetbaguette <yesseniapan@hotmail.com>
2022-10-11 22:44:07 -0500

    pass tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let fToC = (fahrenheit - 32) * (5 / 9);
+  let fToCRounded = Math.round(fToC * 10) / 10;
+  return fToCRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let cToF = (celsius * (9 / 5)) + 32;
+  let cToFRounded = Math.round(cToF * 10) / 10;
+  return cToFRounded;
 };
 
 // Do not edit below this line
