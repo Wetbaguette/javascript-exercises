6962bf6f3afc45ae63678984eddfddb216cb5390
Wetbaguette <yesseniapan@hotmail.com>
2023-02-15 23:32:10 -0500

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let newStr = string.toLowerCase()
+    .replace(/[^\w\s\']|_/g, "")
+    .replace(/\s+/g, "");
+      let arr = [...newStr];
+      let word = arr.reverse().join('');
+    if (word ===  newStr) {
+        return true;
+      } else {
+        return false;
+      }
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
