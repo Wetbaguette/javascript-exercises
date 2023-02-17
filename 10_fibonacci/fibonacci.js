11117d2d066afdf09381b965bb4a6401d478705b
Wetbaguette <yesseniapan@hotmail.com>
2023-02-16 23:08:43 -0500

    easy finish

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr = [];
+    let result;
+    if (num < 0) {
+        return 'OOPS';
+    }
+    for (let i = 0; i <= num; i++){
+      if (arr.length === 0) {
+        arr.push(1);
+        arr.push(1);
+      } else {
+        result = arr[arr.length - 2] + arr[arr.length - 1];
+        arr.push(result);
+      }
+    }
+    return arr[num - 1];
 };
 
 // Do not edit below this line
