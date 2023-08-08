1a87ad780c84ede93fb7cde278f205698a9472a2
Wetbaguette <yesseniapan@hotmail.com>
2023-08-07 22:52:29 -0500

    yrs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,9 @@
-const reverseString = function(string) {
-    reversedStr = "";
-    for (let i = string.length - 1; i >= 0; i--) {
-        reversedStr += string[i]
-    };
-    return reversedStr;
+const reverseString = function (string) {
+	let reversedStr = "";
+	for (let i = string.length - 1; i >= 0; i--) {
+		reversedStr += string[i];
+	}
+	return reversedStr;
 };
 
 // Do not edit below this line
